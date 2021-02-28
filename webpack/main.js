1748f36d4d856db87d245c34a87114b72f37c88e
marina <marssemenova@gmail.com>
2021-02-28 18:27:34 -0500

    books animation

--- webpack/main.js
+++ webpack/main.js
@@ -103,4 +103,25 @@ window.addEventListener("load", () => {
   document.getElementById("userData")?.addEventListener("change", (e) => {
     db.updateProgress({ [e.target.name]: e.target.checked });
   });
+
+  const overlap = document.getElementById("overlap")
+
+  const chemistry = document.getElementById("chemistry")
+  const math = document.getElementById("math")
+  const physics = document.getElementById("physics")
+
+  overlap.onmouseover = function() {
+    chemistry.classList.remove("chemistryTransOut");
+    physics.classList.remove("physicsTransOut");
+    chemistry.classList.add("chemistryTransIn");
+    physics.classList.add("physicsTransIn");
+  }
+
+  overlap.onmouseout = function() {
+    chemistry.classList.remove("chemistryTransIn");
+    physics.classList.remove("physicsTransIn");
+    chemistry.classList.add("chemistryTransOut");
+    physics.classList.add("physicsTransOut");
+  }
 });
+
